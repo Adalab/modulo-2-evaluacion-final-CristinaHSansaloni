50ff7ebfe92b356f83326a9feabebad3f602af02
CristinaH.Sansaloni <aacchhss@hotmail.com>
2022-07-16 19:30:16 +0200

    add minimal structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
+// variables
+const input = document.querySelector('.js_input');
+const btnSubmit = document.querySelector('.js_submit');
+const btnReset = document.querySelector('.js_reset');
+const resultsList = document.querySelector('.js_results_list');
+const favoritesList = document.querySelector('.js_favorites_list');
 
+//petition to server
